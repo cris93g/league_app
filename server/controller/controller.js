3c3d67308d60d8dc5f172854ff7a9ccd7c053159
cris93g <37388115+cris93g@users.noreply.github.com>
2018-08-27 01:30:39 -0500

    server has all of champions

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,22 +1,17 @@
 const axios = require("axios");
-
 let champs = [];
-let id = 0;
 axios
   .get("http://ddragon.leagueoflegends.com/cdn/8.14.1/data/en_US/champion.json")
   .then(response => {
-    let arr = response.data.champs;
-
-    let objectWithIds = arr.map(obj => {
-      obj.id = id;
-      id++;
-      return obj;
-    });
-    champs = objectWithIds;
+    champs = response.data.data;
+    // console.log(champs);
+    return champs;
   });
+console.log(champs);
 
 let getChamps = (req, res) => {
   res.status(200).send(champs);
+  console.log(champs);
 };
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const port = process.env.port || 3001;
 // app.use(cors());
 app.use(json());
 
-app.get("api/getChamps", champCtrl.getChamps);
+app.get("/api/getChampions", champCtrl.getChamps);
 
 app.listen(port, () => {
   console.log(`leagues app listening or port ${port}`);
